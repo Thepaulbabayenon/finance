2d3456588057317f0929fbe76241bbc707f006cd
PAUL BABAYEN-ON <138440106+Thepaulbabayenon@users.noreply.github.com>
2024-06-02 01:33:09 +0800

    Update Header

--- components/header.tsx
+++ components/header.tsx
@@ -1,34 +1,42 @@
+"use client";
 import { HeaderLogo } from "@/components/header-logo";
 import { Navigation } from "./navigation";
-import { UserButton, ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
+import { UserButton, ClerkLoading, ClerkLoaded, useUser } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react";
 import { WelcomeMsg } from "./welcome-msg";
+import Link from "next/link";
 import { Filters } from "./filters";
-
-
-
+import { CircleUserRound } from "lucide-react";
 const Header = () => {
+  const { user } = useUser();
   return (
-    <header className='bg-gradient-to-b from-blue-700 to-blue-700
-    px-4 py-8 lg:px-14 pb-36'>
-    <div className='max-w-screen-2xl mx-auto'>
-      <div className="w-full flex items-center justify-center mb-14">
-        <div className='w-full flex items-center lg:gap-16'>
-    <HeaderLogo />
-    <Navigation />
+    <header className="bg-gradient-to-b from-blue-700 to-blue-700 px-4 py-8 lg:px-14 pb-36">
+      <div className="max-w-screen-2xl mx-auto">
+        <div className="w-full flex items-center justify-between mb-14">
+          <div className="w-full flex items-center lg:gap-16">
+            <HeaderLogo />
+            <Navigation />
+          </div>
+          <div>
+            <ClerkLoaded>
+            {user ? (
+          <UserButton afterSignOutUrl="/sign-in" />
+        ) : (
+          <Link href="/sign-in">
+            <CircleUserRound />
+          </Link>
+        )}
+            </ClerkLoaded>
+            <ClerkLoading>
+              <Loader2 className="size-8 animate-spin text-slate-400" />
+            </ClerkLoading>
+          </div>
+        </div>
+        <WelcomeMsg />
+        <Filters />
       </div>
-      <ClerkLoaded>
-      <UserButton afterSignOutUrl="/sign-in" />
-      </ClerkLoaded>
-      <ClerkLoading>
-        <Loader2 className="size-8 animate-spin text-slate-400"/>
-      </ClerkLoading>
-    </div>
-    <WelcomeMsg />
-    <Filters />
- </div>
- </header>
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
